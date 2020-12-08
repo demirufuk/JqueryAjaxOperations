b83e96ed3a1a74aa9825d78aef5aaa8d4f806fc1
Ufuk Demir <38029528+ufukdemir54@users.noreply.github.com>
2020-12-08 15:15:29 +0300

    Jquery Ajax

---
+++ JqueryAjaxOperations/Controllers/EmployeeController.cs
@@ -0,0 +1,113 @@
+﻿using JqueryAjaxOperations.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryAjaxOperations.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Employee.ToList();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+
+            if (id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    emp = db.Employee.Where(x => x.Id == id).FirstOrDefault();
+                }
+            }
+
+            return View(emp);
+
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                using (DBModel db = new DBModel())
+                {
+                    if(emp.Id == 0)
+                    {
+                        db.Employee.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()),
+                    message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "false" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using(DBModel db = new DBModel())
+                {
+                    Employee emp = db.Employee.Where(x => x.Id == id).FirstOrDefault<Employee>();
+                    db.Employee.Remove(emp);
+                    db.SaveChanges();
+                }
+
+                return Json(new
+                {
+                    success = true,
+                    html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()),
+                    message = "Deleted Successfully"
+                }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "false" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ JqueryAjaxOperations/Scripts/Scripts.js
@@ -0,0 +1,85 @@
+﻿
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        };
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        var ajaxConfig =
+        {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.success) {
+
+                    $("#firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true);
+                }
+                else {
+                    //error
+                }
+
+            }
+        };
+
+        if ($(form).attr('enctype') == "mutipart/form-data") {
+            ajaxConfig["contentType"] = false,
+                ajaxConfig["processData"] = false;
+        }
+
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $('#secondTab').html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewTab) {
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+            }
+        }
+    });
+}
+
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $('#secondTab').html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+            $('ul.nav.nav-tabs a:eq(1)').tab('show');
+        }
+    });
+}
+
+function Delete(url) {
+    if (confirm('Silmek istediğinize emin misiniz?') == true) {
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $('#firstTab').html(response.html);
+                }
+            }
+        });
+    }
+}
+
